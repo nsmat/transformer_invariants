a00a566fff8df398396e4b5cfc0cc70a1ec8226c
Niklas Mather <niksmather@gmail.com>
2023-08-04 16:52:50 +0200

    distance

--- utils/transforms.py
+++ utils/transforms.py
@@ -1,10 +1,5 @@
 import torch
-
 import torch_geometric as tg
-
-from torch_geometric.data import Data
-from torch_geometric.data.datapipes import functional_transform
-from torch_geometric.transforms import BaseTransform
 
 
 class OneHot(tg.transforms.BaseTransform):
@@ -22,7 +17,7 @@ class OneHot(tg.transforms.BaseTransform):
         self.name = name
         self.delete_original = delete_original
 
-    def forward(self, data: tg.data.Data) -> tg.data.Data:
+    def __call__(self, data: tg.data.Data) -> tg.data.Data:
         to_cast = getattr(data, self.input)
         one_hot = torch.nn.functional.one_hot(to_cast).float()
         setattr(data, self.name, one_hot)
@@ -37,15 +32,15 @@ class EuclideanInformationTransform(tg.transforms.BaseTransform):
     """Computes distances and relative_positions and stores them in
     corresponding names"""
 
-    def forward(self, data: tg.data.Data) -> tg.data.Data:
+    def __call__(self, data: tg.data.Data) -> tg.data.Data:
         source_nodes = data.edge_index[0, :]
         target_nodes = data.edge_index[1, :]
         relative_positions = data.pos[target_nodes] - data.pos[source_nodes]
 
         distances = torch.norm(relative_positions, p=2, dim=-1).view(-1, 1)
 
-        # Normalise distances to between 0 and 1
-        if distances.numel > 0:
+        # Normalise distances to between 0 and 1 (if nonempty)
+        if distances.numel() > 0:
             max_distance = distances.max()
             distances = distances / max_distance
 
