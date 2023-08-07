156b1488376da09601272f7d0188e34a800eff86
Niklas Mather <niksmather@gmail.com>
2023-08-07 14:39:10 +0200

    nearly at proper test

--- models/attention_network.py
+++ models/attention_network.py
@@ -9,18 +9,19 @@ import e3nn
 
 class GraphAttentionNetwork(tg.nn.MessagePassing):
 
-    def __init__(self, K, Q, V):
+    def __init__(self, K, Q, V, self_loop_attention_value):
         super().__init__(aggr='add')
         self.K = K
         self.Q = Q
         self.V = V
 
+        self.self_loop_attention_value = self_loop_attention_value
 
     # This is split out so that we can overwrite it when testing
     def alpha_normalisation(self, neighbourhood_dot_products):
         return torch.nn.functional.softmax(neighbourhood_dot_products, dim=0)
 
-    def compute_alpha(self, edge_index, k_uv, q, self_loop_attention_value=1.0):
+    def compute_alpha(self, edge_index, k_uv, q, self_loop_attention_value):
         """Creates a matrix of alpha values based on keys and queries"""
         alphas = torch.zeros((q.shape[0], q.shape[0]))
         for node in range(q.shape[0]): # iterate through the nodes
@@ -45,14 +46,17 @@ class GraphAttentionNetwork(tg.nn.MessagePassing):
         return alphas
 
 
-    def forward(self, edge_index, features, geometric_information, distances):
-        k_uv = self.K(edge_index, features, geometric_information, distances)
+    def forward(self, edge_index, features, edge_features, **kwargs):
+        k_uv = self.K(edge_index, features, edge_features, **kwargs)
 
         q = self.Q(features)
-        alpha = self.compute_alpha(edge_index, k_uv, q)
-        v = self.V(edge_index, features, geometric_information, distances)
+        alpha = self.compute_alpha(edge_index, k_uv, q, self.self_loop_attention_value)
+        v = self.V(edge_index, features, edge_features, **kwargs)
 
-        return self.propagate(edge_index, alpha=alpha, v=v)
+        # We add self loops to the index _after_ alphas are computed, since we hardcode the alphas for those
+        looped_edge_index, _ = tg.utils.loop.add_self_loops(edge_index)
+
+        return self.propagate(looped_edge_index, alpha=alpha, v=v)
 
 
     def message(self, alpha, v_j, edge_index):
