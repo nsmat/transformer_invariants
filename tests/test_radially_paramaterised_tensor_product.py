9422077298a95811b529acf8dd61433a0a9bc330
Niklas Mather <niksmather@gmail.com>
2023-08-07 18:09:00 +0200

    clean up tests

--- tests/test_radially_paramaterised_tensor_product.py
+++ tests/test_radially_paramaterised_tensor_product.py
@@ -4,42 +4,6 @@ import e3nn
 
 from unittest import TestCase
 
-
-def compute_equivariance_error_rptp(alpha, beta, gamma, model, feature_irreps, geometric_irreps):
-    random_features = feature_irreps.randn(1, -1)
-    random_geometric = geometric_irreps.randn(1, -1)
-    distances = torch.tensor(1.).unsqueeze(0).unsqueeze(0)  # Add a batch dimension and a node dimension
-
-    # Need to compute one 'rotation matrix' for each set of irreps
-    rotation_matrix_features = feature_irreps.D_from_angles(alpha, beta, gamma)
-    rotated_features = random_features @ rotation_matrix_features
-
-    rotation_matrix_geometric = geometric_irreps.D_from_angles(alpha, beta, gamma)
-    rotated_geometric = random_geometric @ rotation_matrix_geometric
-
-    output = model.forward(random_features.unsqueeze(0).unsqueeze(0),
-                           random_geometric.unsqueeze(0).unsqueeze(0),
-                           distances)
-
-    rotated_output = output @ rotation_matrix_features
-    output_from_rotated_inputs = model.forward(rotated_features.unsqueeze(0).unsqueeze(0),
-                                               rotated_geometric.unsqueeze(0).unsqueeze(0),
-                                               distances)
-
-    error = (rotated_output - output_from_rotated_inputs).pow(2) / rotated_output.pow(2).sum()
-
-    return error
-
-
-def compute_equivariance_error(model, angles, feature_irreps, geometric_irreps):
-    errors = []
-    for alpha, beta, gamma in zip(*angles):
-        error = compute_equivariance_error_rptp(alpha, beta, gamma, model, feature_irreps, geometric_irreps)
-        errors.append(error)
-
-    return torch.concat(errors)
-
-
 class TensorProductEquivarianceTest(TestCase):
     def test_equivariance_rptp(self):
         feature_irreps = e3nn.o3.Irreps("10x0e + 10x1e + 10x2e")
@@ -53,6 +17,39 @@ class TensorProductEquivarianceTest(TestCase):
                                                   )
         angles = e3nn.o3.rand_angles(100)
 
-        errors = compute_equivariance_error(rptp, angles, feature_irreps, geometric_irreps)
+        errors = self.compute_equivariance_error(rptp, angles, feature_irreps, geometric_irreps)
         max_error = errors.max().item()
         self.assertAlmostEquals(max_error, 0)
+
+    def compute_equivariance_error(self, model, angles, feature_irreps, geometric_irreps):
+        errors = []
+        for alpha, beta, gamma in zip(*angles):
+            error = self.compute_equivariance_error_rptp(alpha, beta, gamma, model, feature_irreps, geometric_irreps)
+            errors.append(error)
+
+        return torch.concat(errors)
+
+    def compute_equivariance_error_rptp(self, alpha, beta, gamma, model, feature_irreps, geometric_irreps):
+        random_features = feature_irreps.randn(1, -1)
+        random_geometric = geometric_irreps.randn(1, -1)
+        distances = torch.tensor(1.).unsqueeze(0).unsqueeze(0)  # Add a batch dimension and a node dimension
+
+        # Need to compute one 'rotation matrix' for each set of irreps
+        rotation_matrix_features = feature_irreps.D_from_angles(alpha, beta, gamma)
+        rotated_features = random_features @ rotation_matrix_features
+
+        rotation_matrix_geometric = geometric_irreps.D_from_angles(alpha, beta, gamma)
+        rotated_geometric = random_geometric @ rotation_matrix_geometric
+
+        output = model.forward(random_features.unsqueeze(0).unsqueeze(0),
+                               random_geometric.unsqueeze(0).unsqueeze(0),
+                               distances)
+
+        rotated_output = output @ rotation_matrix_features
+        output_from_rotated_inputs = model.forward(rotated_features.unsqueeze(0).unsqueeze(0),
+                                                   rotated_geometric.unsqueeze(0).unsqueeze(0),
+                                                   distances)
+
+        error = (rotated_output - output_from_rotated_inputs).pow(2) / rotated_output.pow(2).sum()
+
+        return error
